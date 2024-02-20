68e13d81e8ef5753cba49db655b764a7bc7ca3d1
seonyo <jsy6535@naver.com>
2024-02-20 18:11:23 +0900

    Init Commit

---
+++ src/main/java/com/example/Dinothing/DinothingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Dinothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Dinothing/DinothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Dinothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
