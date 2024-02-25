b1f03c3286f66f27a4d67f06c446d4c0c0237e6b
seonyo <jsy6535@naver.com>
2024-02-26 06:43:08 +0900

    Rename: UserDto -> RegisterDto 파일명 변경

--- src/main/java/com/example/Dinothing/dto/RegisterDto.java
+++ src/main/java/com/example/Dinothing/dto/RegisterDto.java
@@ -3,7 +3,7 @@ package com.example.Dinothing.dto;
 import lombok.Getter;
 
 @Getter
-public class UserDto {
+public class RegisterDto {
     private String nickname;
     private String email;
     private String password;
