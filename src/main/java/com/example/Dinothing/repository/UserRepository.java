2913b658289988286027affc52d970557f5f36a2
seonyo <jsy6535@naver.com>
2024-02-29 01:47:00 +0900

    Feat: existsByPassword와 findByEmail 리포지토리에 언급

--- src/main/java/com/example/Dinothing/repository/UserRepository.java
+++ src/main/java/com/example/Dinothing/repository/UserRepository.java
@@ -3,6 +3,10 @@ package com.example.Dinothing.repository;
 import com.example.Dinothing.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     boolean existsByEmail(String email);
+    boolean existsByPassword(String password);
+    Optional<UserEntity> findByEmail(String email);
 }
