80b67d432d633f95667eae51b00b0c5c61e3826a
seonyo <jsy6535@naver.com>
2024-03-13 08:23:33 +0900

    Feat: IdeaDto 생성

---
+++ src/main/java/com/example/Dinothing/dto/IdeaDto.java
@@ -0,0 +1,15 @@
+package com.example.Dinothing.dto;
+
+import lombok.Getter;
+
+@Getter
+public class IdeaDto {
+    private String q1;
+    private String q2;
+    private String q3;
+    private String q4;
+    private String q5;
+    private String q6;
+    private String q7;
+    private String color;
+}
