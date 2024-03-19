fc1bd3c1ab38c63a090d6a4104b8e79f967cf211
seonyo <jsy6535@naver.com>
2024-03-20 01:30:18 +0900

    Feat: IdeaService Bean 등록

--- src/main/java/com/example/Dinothing/config/AppConfig.java
+++ src/main/java/com/example/Dinothing/config/AppConfig.java
@@ -1,6 +1,8 @@
 package com.example.Dinothing.config;
 
+import com.example.Dinothing.repository.IdeaRepository;
 import com.example.Dinothing.repository.UserRepository;
+import com.example.Dinothing.service.IdeaService;
 import com.example.Dinothing.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +14,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class AppConfig {
 
     private final UserRepository userRepository;
+    private final IdeaRepository ideaRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Bean
     public UserService userService() {
         return new UserService(userRepository, passwordEncoder);
+    }
+
+    @Bean
+    public IdeaService ideaService(){
+        return new IdeaService(ideaRepository);
     }
 }
