0f81e2e5bff0747c3d9e07c2ae60bbe635d55b64
seonyo <jsy6535@naver.com>
2024-03-28 23:26:01 +0900

    Remove: UserRepository 필요없는 메소드 삭제

--- src/main/java/com/example/Dinothing/repository/UserRepository.java
+++ src/main/java/com/example/Dinothing/repository/UserRepository.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
-    boolean existsByPassword(String password);
     Optional<UserEntity> findByEmail(String email);
 }
