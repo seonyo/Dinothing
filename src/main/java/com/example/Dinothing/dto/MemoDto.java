dbd6162b19e08276d4f6bc87f44f326e544ff752
seonyo <jsy6535@naver.com>
2024-04-03 01:52:23 +0900

    Feat: MemoDto 생성

---
+++ src/main/java/com/example/Dinothing/dto/MemoDto.java
@@ -0,0 +1,21 @@
+package com.example.Dinothing.dto;
+
+import com.example.Dinothing.entity.MemoEntity;
+import com.example.Dinothing.entity.UserEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemoDto {
+    private UserEntity userId;
+    private String content;
+
+
+    public MemoEntity toEntity() {
+        MemoEntity memoEntity = new MemoEntity();
+        memoEntity.setUser(userId);
+        memoEntity.setContent(content);
+        return memoEntity;
+    }
+}
