5cefb5c2ad7a893dab6fda69016e6abf31070b2e
seonyo <jsy6535@naver.com>
2024-04-03 01:53:06 +0900

    Feat: MemoService 생성

---
+++ src/main/java/com/example/Dinothing/service/MemoService.java
@@ -0,0 +1,26 @@
+package com.example.Dinothing.service;
+
+import com.example.Dinothing.dto.MemoDto;
+import com.example.Dinothing.entity.MemoEntity;
+import com.example.Dinothing.repository.MemoRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class MemoService {
+    private final MemoRepository memoRepository;
+    public MemoEntity createOrUpdateMemo(MemoDto request){
+        Optional<MemoEntity> optionalMemo = memoRepository.findById(request.getUserId().getId());
+
+        MemoEntity memo = optionalMemo.orElseGet(MemoEntity::new);
+
+        memo.setContent(request.getContent());
+
+        return memoRepository.save(memo);
+    }
+}
