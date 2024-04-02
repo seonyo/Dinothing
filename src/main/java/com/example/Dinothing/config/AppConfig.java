37d83aca19a9ef6d4105b6d27a4254976f5cf938
seonyo <jsy6535@naver.com>
2024-04-03 01:56:25 +0900

    Feat: MemoService Bean 등록

--- src/main/java/com/example/Dinothing/config/AppConfig.java
+++ src/main/java/com/example/Dinothing/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.example.Dinothing.config;
 
 import com.example.Dinothing.repository.IdeaRepository;
+import com.example.Dinothing.repository.MemoRepository;
 import com.example.Dinothing.repository.UserRepository;
+import com.example.Dinothing.service.HeartService;
 import com.example.Dinothing.service.IdeaService;
+import com.example.Dinothing.service.MemoService;
 import com.example.Dinothing.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +18,7 @@ public class AppConfig {
 
     private final UserRepository userRepository;
     private final IdeaRepository ideaRepository;
+    private final MemoRepository memoRepository;
     private final PasswordEncoder passwordEncoder;
 
     @Bean
@@ -26,4 +30,7 @@ public class AppConfig {
     public IdeaService ideaService(){
         return new IdeaService(ideaRepository, userRepository);
     }
+
+    @Bean
+    public MemoService memoService(){return new MemoService(memoRepository);}
 }
