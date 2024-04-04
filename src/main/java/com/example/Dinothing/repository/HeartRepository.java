f66cdb93532e9c5ca714eeaaf6d3ddf0bc962993
seonyo <jsy6535@naver.com>
2024-04-04 18:47:38 +0900

    Feat: Heart Repository 생성

---
+++ src/main/java/com/example/Dinothing/repository/HeartRepository.java
@@ -0,0 +1,10 @@
+package com.example.Dinothing.repository;
+
+import com.example.Dinothing.entity.HeartEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HeartRepository extends JpaRepository<HeartEntity, Long> {
+    Optional<HeartEntity> findByUserIdAndIdeaId(Long userId, Long ideaId);
+}
