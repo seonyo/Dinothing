beff6444ba6810a24a5b83c5f79fc4778dfc4a6f
seonyo <jsy6535@naver.com>
2024-04-05 10:32:20 +0900

    Feat: DeleteHeart 메소드 추가

--- src/main/java/com/example/Dinothing/repository/HeartRepository.java
+++ src/main/java/com/example/Dinothing/repository/HeartRepository.java
@@ -2,11 +2,14 @@ package com.example.Dinothing.repository;
 
 import com.example.Dinothing.dto.DeleteHeartDto;
 import com.example.Dinothing.entity.HeartEntity;
+import com.example.Dinothing.entity.IdeaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<HeartEntity, Long> {
     Optional<HeartEntity> findByUserIdAndIdeaId(Long userId, Long ideaId);
     void deleteByUserIdAndIdeaId(Long userId, Long ideaId);
+    List<HeartEntity> findAllByUserId(Long userId);
 }
