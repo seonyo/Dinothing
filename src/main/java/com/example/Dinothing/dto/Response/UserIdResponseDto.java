8322ff29592bfd59116d3db81434915853ad5d62
seonyo <jsy6535@naver.com>
2024-04-07 06:14:03 +0900

    Feat: UserIdResponseDto 생성

--- src/main/java/com/example/Dinothing/dto/Response/UserIdResponseDto.java
+++ src/main/java/com/example/Dinothing/dto/Response/UserIdResponseDto.java
@@ -1,4 +1,13 @@
 package com.example.Dinothing.dto.Response;
 
+import lombok.*;
+
+@Getter
 public class UserIdResponseDto {
+    private final Long id;
+
+    public UserIdResponseDto (Long user){
+        this.id = user;
+    }
+
 }
